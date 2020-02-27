bd0baf89f803c1bb6ee3aaf61a2f48148dd31a73
Scott Swatosh <scott.swatosh@gmail.com>
2020-02-27 12:07:47 -0600

    Set up logging and metrics

--- src/main/java/dev/sswatosh/fridgemanager/Main.java
+++ src/main/java/dev/sswatosh/fridgemanager/Main.java
@@ -3,14 +3,23 @@ package dev.sswatosh.fridgemanager;
 import dev.sswatosh.fridgemanager.config.MainModule;
 import dev.sswatosh.fridgemanager.handlers.HelloNameHandler;
 import dev.sswatosh.fridgemanager.handlers.HelloWorldHandler;
+import ratpack.dropwizard.metrics.DropwizardMetricsModule;
 import ratpack.guice.Guice;
+import ratpack.handling.RequestLogger;
 import ratpack.server.RatpackServer;
 
 public class Main {
     public static void main(String... args) throws Exception {
         RatpackServer.start(server -> server
-                .registry(Guice.registry(bindings -> bindings.module(MainModule.class)))
+                .registry(Guice.registry(bindings ->
+                        bindings.module(MainModule.class)
+                                .module(new DropwizardMetricsModule(),
+                                        c -> c.jvmMetrics(true)
+                                              .jmx()
+                                )
+                ))
                 .handlers(chain -> chain
+                        .all(RequestLogger.ncsa())
                         .get(HelloWorldHandler.class)
                         .get(":name", HelloNameHandler.class)
                 )
--- src/main/java/dev/sswatosh/fridgemanager/handlers/HelloWorldHandler.java
+++ src/main/java/dev/sswatosh/fridgemanager/handlers/HelloWorldHandler.java
@@ -1,11 +1,16 @@
 package dev.sswatosh.fridgemanager.handlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 
 public class HelloWorldHandler implements Handler {
+    Logger logger = LoggerFactory.getLogger(HelloWorldHandler.class);
+
     @Override
     public void handle(Context ctx) throws Exception {
         ctx.render("Hello World!");
+        logger.info("Hello logs!");
     }
 }
